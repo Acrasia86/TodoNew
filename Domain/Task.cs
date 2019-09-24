2b09eed90f7f72904b12c1e5b9ed797fbd416c45
Jacob <Jacob.pahlsson@hotmail.com>
2019-09-24 10:51:49 +0200

    Add Constructor,  Todo method. Program.cs, Task.cs

---
+++ Domain/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoNew.Domain
+{
+    class Task
+    {
+        public Task()
+        {
+
+        }
+
+        public Task(string title, DateTime dueDate)
+        {
+            Title = title;
+            DueDate = dueDate;
+        }
+
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,81 @@
 ﻿using System;
+using TodoNew.Domain;
 
 namespace TodoNew
 {
+
     class Program
     {
+        static Task[] tasks = new Task[100];
         static void Main(string[] args)
         {
             bool shouldNotExit = true;
-
-            Console.Clear();
+  
+            
 
             while (shouldNotExit)
             {
+                Console.Clear();
                 Console.WriteLine("1. Add todo");
                 Console.WriteLine("2. List todo");
                 Console.WriteLine("3. Exit");
 
                 int userInput = int.Parse(Console.ReadLine());
+                Console.Clear();
 
                 switch (userInput)
                 {
                     case 1:
-                        AddTodo();
+
+                        Console.Write("Title: ");
+                        string title = Console.ReadLine();
+
+                        Console.Write("Due date (yyyy-mm-dd-hh:mm): ");
+                        DateTime dueDate = DateTime.Parse(Console.ReadLine());
+
+                        tasks[GetIndexPosition()] = new Task(title, dueDate);
+
                         break;
+
+
                     case 2:
-                        ListTodo();
+                        
                         break;
                     case 3:
                         shouldNotExit = false;
                         break;
+
+                }
+
+                
+            }
+            
+        }
+
+
+        static int GetIndexPosition()
+        {
+            int result = -1;
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                if (tasks[i] != null)
+                {
+                    continue;
+                }
+                if (tasks[i] == null)
+                {
+                    result = i;
+                    break;
+                }
+                if (result == -1)
+                {
+                    throw new Exception("No avalible position");
                 }
             }
+            return result;
         }
     }
+   
 }
+
+
