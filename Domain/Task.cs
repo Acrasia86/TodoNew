d7c1304137e2a0d0d9ed9b142289d5de907dbaf4
Jacob <Jacob.pahlsson@hotmail.com>
2019-09-24 13:22:22 +0200

    Finnished

--- Domain/Task.cs
+++ Domain/Task.cs
@@ -11,25 +11,27 @@ namespace TodoNew.Domain
 
         }
 
-        public Task(string title, DateTime dueDate)
+        public Task(int id, string title, DateTime dueDate)
         {
             Title = title;
             DueDate = dueDate;
+            Id = id;
         }
-
         
+        public int Id { get; set; }
         public DateTime? DueDate { get; set; }
         public DateTime? Completed { get; set; }
         public string Title { get; set; }
-        public bool IsCompleted
-        {
-            get
-            {
 
-            return Completed != null;
+        //public bool IsCompleted
+        //{
+        //    get
+        //    {
+
+        //    return Completed != null;
  
-            }
-        }
+        //    }
+        //}
         
 
     }
--- Program.cs
+++ Program.cs
@@ -7,14 +7,16 @@ namespace TodoNew
     class Program
     {
         static Task[] tasks = new Task[100];
+        
         static void Main(string[] args)
         {
+            int id = 1;
             bool shouldNotExit = true;
-  
-            
 
             while (shouldNotExit)
             {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                
                 Console.Clear();
                 Console.WriteLine("1. Add todo");
                 Console.WriteLine("2. List todo");
@@ -26,14 +28,14 @@ namespace TodoNew
                 switch (userInput)
                 {
                     case 1:
-
+                        Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.Write("Title: ");
                         string title = Console.ReadLine();
 
                         Console.Write("Due date (yyyy-mm-dd-hh:mm): ");
                         DateTime dueDate = DateTime.Parse(Console.ReadLine());
 
-                        tasks[GetIndexPosition()] = new Task(title, dueDate);
+                        tasks[GetIndexPosition()] = new Task(id++, title, dueDate);
 
                         break;
 
@@ -44,10 +46,11 @@ namespace TodoNew
                         {
                             if(todos != null)
                             {
-                                Console.WriteLine($"{todos.Title} {todos.DueDate}");
+                                Console.WriteLine($"Id: {todos.Id}\nTitle: {todos.Title}\nDue date: {todos.DueDate}\nCompleted on: {todos.Completed}.\n");
                             }
                         }
-                        Console.ReadLine();
+                        
+                        ConsoleKeyInfo keyPress = Console.ReadKey(true);
                         
                         break;
                     case 3:
