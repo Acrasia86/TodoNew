940766b84d9a3c50b783ecb50835c2496b3f4f2a
Jacob <Jacob.pahlsson@hotmail.com>
2019-09-24 09:42:11 +0200

    Add Menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace TodoNew
     {
         static void Main(string[] args)
         {
-            
+            bool shouldNotExit = true;
+
+            Console.Clear();
+
+            while (shouldNotExit)
+            {
+                Console.WriteLine("1. Add todo");
+                Console.WriteLine("2. List todo");
+                Console.WriteLine("3. Exit");
+
+                int userInput = int.Parse(Console.ReadLine());
+
+                switch (userInput)
+                {
+                    case 1:
+                        AddTodo();
+                        break;
+                    case 2:
+                        ListTodo();
+                        break;
+                    case 3:
+                        shouldNotExit = false;
+                        break;
+                }
+            }
         }
     }
 }
